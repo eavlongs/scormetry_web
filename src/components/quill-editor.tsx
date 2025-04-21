362a4ce8de8c4b607e4fa33d0d2cd942ab62568a
eavlongs <eavlongs@gmail.com>
2025-04-21 23:47:34 +0700

    updated quilljs editor

--- src/components/quill-editor.tsx
+++ src/components/quill-editor.tsx
@@ -1,41 +1,47 @@
 'use client'
 
 import { cn } from '@/lib/utils'
+import { Delta } from 'quill'
 import { useEffect, useRef, useState } from 'react'
 
 interface QuillEditorProps {
-    initialContent?: string
+    initialContent?: Delta
     readOnly?: boolean
-    onContentChange?: (content: string) => void
+    onContentChange?: (content: Delta) => void
     placeholder?: string
+    className?: string
 }
 
 const quillEditorElementId = 'quill-editor'
 const quillToolbarQuerySelectorString = '[role="toolbar"].ql-toolbar.ql-snow'
 
 export default function QuillEditor({
-    initialContent = '',
+    initialContent,
     readOnly = false,
     onContentChange,
     placeholder,
+    className,
 }: QuillEditorProps) {
     const containerRef = useRef<HTMLDivElement>(null)
     const toolbarRef = useRef<HTMLDivElement>(null)
     const [quill, setQuill] = useState<any>(null)
     const [isLoaded, setIsLoaded] = useState(false)
-    const [content, setContent] = useState('')
 
     useEffect(() => {
-        if (typeof window !== 'undefined' && !isLoaded) {
+        if (
+            typeof window !== 'undefined' &&
+            typeof document !== 'undefined' &&
+            !isLoaded
+        ) {
             // Dynamically import Quill to avoid SSR issues
             import('quill').then((Quill) => {
                 if (containerRef.current) {
-                    const child = containerRef.current.querySelector(
+                    const toolbar = containerRef.current.querySelector(
                         quillToolbarQuerySelectorString
                     )
 
-                    if (child) {
-                        child.remove()
+                    if (toolbar) {
+                        toolbar.remove()
                     }
                 }
                 const quillInstance = new Quill.default('#quill-editor', {
@@ -44,12 +50,14 @@ export default function QuillEditor({
                         toolbar: readOnly
                             ? false
                             : [
-                                  [{ header: [1, 2, 3, 4, 5, 6, false] }],
                                   ['bold', 'italic', 'underline', 'strike'],
                                   [{ color: [] }, { background: [] }],
-                                  [{ list: 'ordered' }, { list: 'bullet' }],
-                                  [{ align: [] }],
-                                  ['link', 'image'],
+                                  [
+                                      { list: 'ordered' },
+                                      { list: 'bullet' },
+                                      { list: 'check' },
+                                  ],
+                                  ['link'],
                                   ['clean'],
                               ],
                     },
@@ -59,13 +67,35 @@ export default function QuillEditor({
 
                 setQuill(quillInstance)
 
+                if (initialContent) {
+                    if (quillInstance.editor.isBlank()) {
+                        // i think quilljs has a bug that renders that it doesn't properly cleanup the component during useEffect strict mode, so we need to check whether it is blank first, to insert initial content
+                        quillInstance.setContents(initialContent)
+                    }
+                }
+
                 // Listen for text changes
                 if (!readOnly) {
-                    quillInstance.on('text-change', () => {
-                        if (onContentChange) {
-                            onContentChange(quillInstance.root.innerHTML)
-                        }
-                    })
+                    if (onContentChange) {
+                        quillInstance.on('text-change', () => {
+                            console.log(quillInstance.editor.getDelta())
+                            onContentChange(quillInstance.editor.getDelta())
+                        })
+                    }
+
+                    const quillEditorContainer =
+                        document.getElementById(quillEditorElementId)
+
+                    // if (quillEditorContainer) {
+                    //     quillEditorContainer.addEventListener('click', () => {
+                    //         const actualQuillEditorInputElement =
+                    //             quillEditorContainer?.querySelector(
+                    //                 '.ql-editor.ql-blank'
+                    //             ) as HTMLDivElement
+
+                    //         actualQuillEditorInputElement?.focus()
+                    //     })
+                    // }
                 }
 
                 setIsLoaded(true)
@@ -94,11 +124,28 @@ export default function QuillEditor({
     }, [isLoaded])
 
     return (
-        <div ref={containerRef}>
+        <>
             <div
-                id={quillEditorElementId}
-                className={cn(readOnly ? 'read-only-editor' : 'min-h-[100px]')}
-            />
-        </div>
+                className={cn(
+                    'flex items-center justify-center min-h-[100px] border',
+                    isLoaded && 'hidden',
+                    className
+                )}
+            >
+                Loading...
+            </div>
+            <div ref={containerRef}>
+                <div
+                    id={quillEditorElementId}
+                    className={cn(
+                        readOnly
+                            ? 'read-only-editor'
+                            : 'min-h-[100px] border flex flex-col',
+                        !isLoaded && 'hidden',
+                        className
+                    )}
+                ></div>
+            </div>
+        </>
     )
 }
